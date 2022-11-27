b4f7013b65362544ee98913df3c1aebded12d7cd
MaksimBozhko <bochkomax2000@gmail.com>
2022-11-27 17:14:47 +0300

    change in new branch

---
+++ src/Components/forNewBranch.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Branch extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Погодное приложение</h1>
+                <p>Узнайте погоду в вашем городе!</p>
+            </div>
+        )
+    }
+}
+
+export default Branch
