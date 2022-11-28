9f2f645b7604c499e4499160fd8723cdb0ac7b6d
MaksimBozhko <bochkomax2000@gmail.com>
2022-11-28 14:18:37 +0300

    new feature

--- src/Components/forNewBranch.js
+++ src/Components/forNewBranch.js
@@ -10,11 +10,7 @@ class NewBranch extends React.Component {
                 <div>new commit developer4</div>
                 <h1>Погодное приложение</h1>
                 <p>Узнайте погоду в вашем городе!</p>
-                <div>developer/login 1</div>
-                <div>developer/login 2</div>
-                <div>developer/popup 1</div>
-                <div>developer/popup 2</div>
-                <div>developer</div>
+                <div>new feature</div>
             </div>
         )
     }
