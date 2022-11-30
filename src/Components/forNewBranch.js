43c7ef98f781f4d3680c2f10f9116c130665d633
MaksimBozhko <bochkomax2000@gmail.com>
2022-11-30 14:20:38 +0300

    add fet1

--- src/Components/forNewBranch.js
+++ src/Components/forNewBranch.js
@@ -18,6 +18,7 @@ class NewBranch extends React.Component {
                 <div>pas</div>
                 <div>pas</div>
                 <div>console</div>
+                <div>this is fit 1</div>
             </div>
         )
     }
