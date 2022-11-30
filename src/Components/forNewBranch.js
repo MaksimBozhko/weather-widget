11ad2f04850f28e8decd5b554f61af28182582e3
MaksimBozhko <bochkomax2000@gmail.com>
2022-11-30 14:06:44 +0300

    add pas

--- src/Components/forNewBranch.js
+++ src/Components/forNewBranch.js
@@ -11,6 +11,9 @@ class NewBranch extends React.Component {
                 <h1>Погодное приложение</h1>
                 <p>Узнайте погоду в вашем городе!</p>
                 <div>feautre1</div>
+                <div>pas</div>
+                <div>pas</div>
+                <div>pas</div>
             </div>
         )
     }
