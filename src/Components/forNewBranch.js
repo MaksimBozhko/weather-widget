592006533c9aedd0d6873efcc395d1cb4d59bfbf
MaksimBozhko <bochkomax2000@gmail.com>
2022-11-30 14:01:59 +0300

    add log

--- src/Components/forNewBranch.js
+++ src/Components/forNewBranch.js
@@ -11,6 +11,9 @@ class NewBranch extends React.Component {
                 <h1>Погодное приложение</h1>
                 <p>Узнайте погоду в вашем городе!</p>
                 <div>feautre1</div>
+                <div>log</div>
+                <div>log</div>
+                <div>log</div>
             </div>
         )
     }
