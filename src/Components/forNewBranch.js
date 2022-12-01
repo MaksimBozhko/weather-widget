e9a2419908adf26fe480e520ec87baeb98769ae9
MaksimBozhko <bochkomax2000@gmail.com>
2022-12-01 13:32:11 +0300

    commit 7

--- src/Components/forNewBranch.js
+++ src/Components/forNewBranch.js
@@ -10,6 +10,7 @@ class NewBranch extends React.Component {
                 <div>commit 4</div>
                 <div>commit 5</div>
                 <div>commit 6</div>
+                <div>commit 7</div>
             </div>
         )
     }
