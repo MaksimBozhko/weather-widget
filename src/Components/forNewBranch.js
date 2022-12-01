ed41bd2b6b192b0524883f2b719edb1b6f552f61
MaksimBozhko <bochkomax2000@gmail.com>
2022-12-01 12:56:36 +0300

    del

--- src/Components/forNewBranch.js
+++ src/Components/forNewBranch.js
@@ -4,25 +4,6 @@ class NewBranch extends React.Component {
     render() {
         return(
             <div>
-                <div>new commit developer</div>
-                <div>new commit developer2</div>
-                <div>new commit developer3</div>
-                <div>new commit developer4</div>
-                <h1>Погодное приложение</h1>
-                <p>Узнайте погоду в вашем городе!</p>
-                <div>feautre1</div>
-                <div>log</div>
-                <div>log</div>
-                <div>log</div>
-                <div>pas</div>
-                <div>pas</div>
-                <div>pas</div>
-                <div>console</div>
-                <div>this is a new helper</div>
-                <div>this is fit 1</div>
-                <div>this is fet2</div>
-                <div>work</div>
-                <div>now work</div>
             </div>
         )
     }
